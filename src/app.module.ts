1597be1f00bf4b06844e660b21e5b307fba0cb51
yousuf141 <77011760+yousuf141@users.noreply.github.com>
2023-05-01 03:46:15 +0100

    set port via env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemGateway } from './item/item.gateway';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, ItemGateway],
 })
